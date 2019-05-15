c3ceae63cf52e280db68a5041a676445c43b1f44
oldbob12 <codecastmodding@gmail.com>
2019-05-15 18:28:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,17 @@ def main():
 	
 	pygame.init()
 	
-	textinput = pygame_textinput.TextInput('','texas_instruments_ti84_series.ttf',64)
+	textinput = pygame_textinput.TextInput('','texas_instruments_ti84_series.ttf',64,0,(72,77,62))
 	
 	white = (255, 255, 255) 
 	green = (0, 255, 0) 
 	blue = (0, 0, 128) 
 
+	lineEquations = []
+	lineAnswers = []
+	lineNum = 1
 
-	numIncrease = 0
-
-	display_surface = pygame.display.set_mode((400, 400 )) 
+	display_surface = pygame.display.set_mode((400, 400 ), pygame.FULLSCREEN) 
 	font = pygame.font.Font('texas_instruments_ti84_series.ttf', 64)
 	
 	
@@ -31,26 +32,34 @@ def main():
 	screen = pygame.display.set_mode((displayWidth,DisplayHeight))
 	clock = pygame.time.Clock()
 	
-	text = font.render('', True, (0,0,0) )
+	text = font.render('', True, (72,77,62) )
 	
 	running = True
 	
 	while running:
-		display_surface.fill(white) 
+		display_surface.fill((149, 161, 128) ) 
 		events = pygame.event.get()
 		for event in events:
 			if event.type == pygame.QUIT:
 				exit()
 				
 		if textinput.update(events):
-			print(textinput.get_text())
-			text = font.render(str(evalCalculator(textinput.get_text())), True, (0,0,0) )
+			lineEquations.append(textinput.get_text())
+			print(lineEquations)
+			lineAnswers.append(evalCalculator(textinput.get_text()))
+			print(lineAnswers)
+			lineNum =+ 1
 			
-		text_rect = text.get_rect()	
-		text_rect.right = displayWidth
-		text_rect.bottom = 10 + (64 *2)
-		
-		display_surface.blit(text,text_rect) 
+		i = 0
+		while i < len(lineAnswers):
+			text = font.render(str(lineAnswers[i]), True, (72,77,62) )
+			text_rect = text.get_rect()	
+			text_rect.right = displayWidth
+			text_rect.bottom = 138 + (128 * i)
+			display_surface.blit(text,text_rect) 
+			i += 1
+			
+			
 		
 			# Blit its surface onto the screen
 		screen.blit(textinput.get_surface(), (10, 10))
