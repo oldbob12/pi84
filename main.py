742b118d98cb65331232dcb6ed85a57d2b9164ec
oldbob12 <codecastmodding@gmail.com>
2019-05-22 20:30:56 -0400

    Update main.py
    Completed text highlighting

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ def main():
 		xCor = []
 		yCor = []
 		textLength = []
+		lineSelect = 0
 	class Answers:
 		line = []
 		xCor = []
 		yCor = []
 		textLength = []
+		lineSelect = 0
 		
 	lineNum = 0
 	lineSelect = 0
@@ -54,23 +56,44 @@ def main():
 				exit()
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_UP:
-					if lineSelect < len(Answers.line)*2:
-						lineSelect+=1
-					print(lineSelect)
+					if lineSelect == 0:
+						if Answers.lineSelect < len(Answers.line):
+							Answers.lineSelect += 1
+							lineSelect = 1
+					else:
+						if Equations.lineSelect < len(Answers.line):
+							Equations.lineSelect += 1
+							lineSelect = 0
 				if event.key == pygame.K_DOWN:
-					if lineSelect > 0:
-						lineSelect-=1
-					print(lineSelect*2)
+					if lineSelect == 1:
+						if Answers.lineSelect > 0:
+							Answers.lineSelect -= 1
+							lineSelect = 0
+					else:
+						if Equations.lineSelect > 0:
+							Equations.lineSelect -= 1
+							lineSelect = 1
+				if(event.key == pygame.K_RETURN):
+					if lineSelect == 1 and Answers.lineSelect > 0:
+						textinput = pygame_textinput.TextInput(textinput.get_text()+str(Answers.line[len(Answers.line) - Answers.lineSelect]),'texas_instruments_ti84_series.ttf',64,0,(0,0,0))
+					if lineSelect == 0 and Equations.lineSelect > 0:
+						textinput = pygame_textinput.TextInput(textinput.get_text()+str(Equations.line[len(Equations.line) - Equations.lineSelect]),'texas_instruments_ti84_series.ttf',64,0,(0,0,0))
+					Equations.lineSelect = 0
+					Answers.lineSelect = 0
+					lineSelect = 0
+
 					
-		if textinput.update(events) and textinput.get_text() != '':
-			lineSelect = 0
+		if textinput.update(events) and textinput.get_text() != '' and Equations.lineSelect == 0 and Answers.lineSelect == 0:
+			Answers.lineSelect = 0
 			Equations.line.append(textinput.get_text())
 			ReturnValue = textinput.get_text()
 			ReturnValue = ReturnValue.replace("^", "**")
 			Answers.line.append(evalCalculator(ReturnValue))
 			textinput = pygame_textinput.TextInput('','texas_instruments_ti84_series.ttf',64,0,(0,0,0))
 			lineNum+=1
+			Equations.xCor = [0]*len(Answers.line)
 			Equations.yCor = [0]*len(Answers.line)
+			Equations.textLength = [0]*len(Answers.line)
 			Answers.xCor = [0]*len(Answers.line)
 			Answers.yCor = [0]*len(Answers.line)
 			Answers.textLength = [0]*len(Answers.line)
@@ -83,8 +106,19 @@ def main():
 					Answers.yCor[i] = 77+ (135 * i)  - (lineNum - 3) * 135
 					Equations.yCor[i] = 12 + (135 * i) - (lineNum - 3) * 135
 				i+=1
-		if(lineSelect > 0 ):
-			pygame.draw.rect(display_surface,(25,86,105),(Answers.xCor[len(Answers.line)-lineSelect]-4,Answers.yCor[len(Answers.line)-lineSelect]-4, Answers.textLength[len(Answers.line)-lineSelect],65))
+		if(Answers.lineSelect > 0):
+			if lineSelect == 1:
+				pygame.draw.rect(display_surface,(25,86,105),
+				(Answers.xCor[len(Answers.line)-Answers.lineSelect]-4,
+				Answers.yCor[len(Answers.line)-Answers.lineSelect]-4, 
+				Answers.textLength[len(Answers.line)-Answers.lineSelect],65))
+		if(Equations.lineSelect > 0):
+			if lineSelect == 0:
+				pygame.draw.rect(display_surface,(25,86,105),
+				(Equations.xCor[len(Equations.line)-Equations.lineSelect]-4,
+				Equations.yCor[len(Equations.line)-Equations.lineSelect]-4, 
+				Equations.textLength[len(Equations.line)-Equations.lineSelect],65))
+				
 		i = 0
 		while i < len(Answers.line):
 			textLineAnswers = font.render(str(Answers.line[i]), True, (0,0,0) )
@@ -100,6 +134,8 @@ def main():
 			display_surface.blit(textLineAnswers,textLineAnswers_rect)
 			Answers.textLength[i] = textLineAnswers.get_width()
 			Answers.xCor[i] = textLineAnswers_rect.x
+			Equations.textLength[i] = textLineEquations.get_width()
+			Equations.xCor[i] = textLineEquations_rect.x
 			i += 1
 	
 		
